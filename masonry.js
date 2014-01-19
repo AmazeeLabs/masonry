237dccaf7946ac890ceec8332a1881259527bdf8
Luxian <lucian.neag@amazee.com>
2014-01-19 23:48:09 +0100

    Adding support for Masonry v3 (incomplete) + fix some upgrade problems (attribute names, function names and library changes)

--- masonry.js
+++ masonry.js
@@ -28,7 +28,7 @@ Drupal.behaviors.masonry = {
           };
         }
       }
-      $options.gutterWidth = settings.gutter_width;
+      $options.gutter = settings.gutter_width;
       $options.isResizable = settings.resizable;
       if (settings.resizable) {
         $options.isAnimated = settings.animated;
@@ -46,16 +46,35 @@ Drupal.behaviors.masonry = {
       }
 
       // Apply Masonry to container
+      var masonry_enabled = Masonry.data($container.get(0));
+
+      // By default, Masonry 3 new items appended to container even if we call
+      // layout(). We need to add them first
+      if (masonry_enabled) {
+        var new_elements = [];
+
+        $($options.itemSelector, $container).each(function(i){
+          if (typeof $container.masonry('getItem', this).element === 'undefined') {
+            new_elements.push(this);
+          }
+        });
+
+        if (new_elements.length) {
+          $container.masonry('appended', new_elements);
+          new_elements.length = 0;
+        }
+      }
+
+      // Init mansory or call layout again if new
       if (settings.images_first) {
         $container.imagesLoaded(function () {
-          $container.hasClass('masonry') ? $container.masonry('reload') : $container.masonry($options);
+          masonry_enabled ? $container.masonry('layout') : $container.masonry($options);
         });
       }
       else {
-        $container.hasClass('masonry') ? $container.masonry('reload') : $container.masonry($options);
+        masonry_enabled ? $container.masonry('layout') : $container.masonry($options);
       }
     });
-
   }
 };
 
